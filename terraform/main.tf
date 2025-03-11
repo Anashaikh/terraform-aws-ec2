78fb5d21b415cc4cafa30269854518279afff6dd
Anashaikh <anaslatif@gmail.com>
2025-03-11 00:14:31 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,53 +30,54 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+ 
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
 
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16" # Replace with your desired CIDR
   tags = {
-    Name = "HelloWorld"
+    Name = "My VPC"
   }
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
-
+# Subnet
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24" # Replace with your desired CIDR
   tags = {
-    Name = "tf-example"
+    Name = "My Subnet"
   }
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+# Security Group
+resource "aws_security_group" "example" {
+  name        = "example-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
 
-  tags = {
-    Name = "tf-example"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Adjust as needed
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
-
+# EC2 Instance
+resource "aws_instance" "example" {
+     ami           = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro" # Replace with your desired instance type
+  subnet_id                   = aws_subnet.main.id # Reference the subnet ID
+  vpc_security_group_ids      = [aws_security_group.example.id] # Reference the security group
   tags = {
-    Name = "primary_network_interface"
-  }
-}
-
-resource "aws_instance" "foo" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
-
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
+    Name = "My EC2 Instance"
   }
 }
